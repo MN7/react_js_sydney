f1df5cfe4f7ff1ce59c00bfe39359674eb2cd3b5
MN7 <narayan.m@gmail.com>
2020-06-12 11:58:24 +1000

    initial commit

---
+++ sampleapps/components/main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class main extends Component {
+  render() {
+    return (
+      abcd
+    );
+  }
+}
+
+export default main;
---
+++ sampleapps/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import main from "../components/main.js"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <main />
+      </div>
+    );
+  }
+}
+
+export default App;
